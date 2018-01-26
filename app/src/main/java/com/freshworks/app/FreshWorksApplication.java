11a98b6c64640f52ad76dc73c6ba2b650e6556c1
Usama Aftab <uaftab@carmanah.com>
2018-01-25 21:24:46 -0800

    First commit

---
+++ app/src/main/java/com/freshworks/app/FreshWorksApplication.java
@@ -0,0 +1,22 @@
+package com.freshworks.app;
+
+import android.app.Application;
+
+/**
+ * Created by Usama Aftab on 2018-01-25.
+ */
+
+public class FreshWorksApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // Put stuff that you want to do at the application startup.
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        // How to handle low memory situation. Kill Quartz job, if any.
+    }
+}
---
+++ app/src/main/java/com/freshworks/app/services/GiphyService.java
@@ -0,0 +1,10 @@
+package com.freshworks.app.services;
+
+/**
+ * Created by Usama Aftab on 2018-01-25.
+ */
+
+public class GiphyService {
+
+
+}
